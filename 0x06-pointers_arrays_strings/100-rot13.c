0e3fea9e1bcec8353e58f4b178a0ff66df945bc5
zigamak <zigamakinde@gmail.com>
2022-07-15 23:04:26 +0100

    cap string

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,67 @@
+#include "main.h"
+<<<<<<< HEAD
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+void print_number(int n)
+{
+	unsigned int m, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+=======
+/**
+ * rot13- encodes a string using rot13
+ * @s: string to encode
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M'};
+
+	int i, j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i])
+			i++;
+		if (s[j] == a[i])
+			s[j] = b[i];
+	}
+	return (s);
+>>>>>>> 60c6948fc99f4ab0a17edd5c17ea6d8b486dc819
+}
